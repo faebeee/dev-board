a245fb88df6d8202b843fd37e0d2c80a46d5bc9d
fabs <faebeee@gmail.com>
2024-11-14 11:12:14 +0100

    Remove conditional import logic in getConfig function
    This commit simplifies the getConfig function by removing the conditional check for the EDGE_CONFIG environment variable and the subsequent dynamic import of a local config file. Now, the function always retrieves the 'dashboards' configuration from the '@vercel/edge-config' package. This change makes the configuration retrieval process more straightforward and consistent.

--- lib/get-config.ts
+++ lib/get-config.ts
@@ -3,13 +3,10 @@ import {get} from '@vercel/edge-config';
 
 
 export const getConfig = async () => {
-  if (process.env.EDGE_CONFIG) {
     const dashboards = await get('dashboards') as Dashboard[];
     if (!dashboards) {
       throw new Error('Config is missing')
     }
 
     return dashboards;
-  }
-  return ((await import('../data/config.json')).default as Dashboard[]);
 }
