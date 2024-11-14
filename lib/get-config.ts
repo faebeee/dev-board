364188674c466dd3dd6210b6bc8bc7e4672d47a2
fabs <faebeee@gmail.com>
2024-11-14 11:10:26 +0100

    Add support for Vercel Edge Config in getConfig
    Integrated Vercel Edge Config to fetch dashboard configurations when EDGE_CONFIG environment variable is set. This enhances configuration management by leveraging the edge-config package. Updated dependencies in package.json and package-lock.json accordingly.

--- lib/get-config.ts
+++ lib/get-config.ts
@@ -1,5 +1,15 @@
 import {Dashboard} from '@/types/widgets';
+import {get} from '@vercel/edge-config';
+
 
 export const getConfig = async () => {
+  if (process.env.EDGE_CONFIG) {
+    const dashboards = await get('dashboards') as Dashboard[];
+    if (!dashboards) {
+      throw new Error('Config is missing')
+    }
+
+    return dashboards;
+  }
   return ((await import('../data/config.json')).default as Dashboard[]);
 }
