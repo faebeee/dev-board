91966c2e1632d9de00b3e5d18b57f234701b36d6
fabs <faebeee@gmail.com>
2024-11-24 18:28:27 +0100

    Remove production config file and improve getConfig error handling.
    Deleted the `config_prod.json` file as it is no longer needed. Enhanced the `getConfig` function to properly handle errors by adding a fallback in case both configurations are missing.

--- lib/get-config.ts
+++ lib/get-config.ts
@@ -1,18 +1,22 @@
-import {Dashboard} from '@/types/widgets';
-import {get} from '@vercel/edge-config';
-import {ConfigType} from '@/lib/config-schema';
+import { ConfigType } from '@/lib/config-schema';
+import { Dashboard } from '@/types/widgets';
+import { get } from '@vercel/edge-config';
 
 
-export const getConfig = async ():Promise<ConfigType> => {
+export const getConfig = async (): Promise<ConfigType> => {
   try {
     const dashboards = await get('dashboards') as Dashboard[];
     if (!dashboards) {
-      throw new Error('Config is missing')
+      throw new Error('Config is missing');
     }
 
     return dashboards;
   } catch {
-    return (await import('../data/config.json')).default;
-    // return (await import('../data/config_prod.json')).default;
   }
-}
+
+  try {
+    return (await import('../data/config.json')).default;
+  } catch {
+    return [];
+  }
+};
