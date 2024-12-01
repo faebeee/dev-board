0e676c1a3745269cf55abe25d315226b200eabdd
fabs <faebeee@gmail.com>
2024-12-01 12:16:37 +0100

    Fix incorrect prop reference in Dashboard component
    Correct the 'type' property reference from a hardcoded 'dashboard' variable to dynamically access it from the first 'dashboards' array element. This ensures the Dashboard component receives the accurate type value associated with the active dashboard.

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
       <Header title={'Home'}/>
       <Suspense>
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 pt-10">
-          {dashboards?.[0]?.widgets && <Dashboard type={dashboard.type} dashboardId={dashboards[0].id} widgets={dashboards[0].widgets!}/>}
+          {dashboards?.[0]?.widgets && <Dashboard type={dashboards[0].type} dashboardId={dashboards[0].id} widgets={dashboards[0].widgets!}/>}
         </div>
       </Suspense>
     </>
